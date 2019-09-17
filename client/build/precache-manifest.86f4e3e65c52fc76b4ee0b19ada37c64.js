6c89a558d086777fd2533b3b38883adc7f8fc7ca
brittmagee <brittgmagee@gmail.com>
2019-09-17 09:36:19 -0400

    Saving initial files

---
+++ client/build/precache-manifest.86f4e3e65c52fc76b4ee0b19ada37c64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed412f494335e1817548b273289623de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91c00eb92e61c3094822",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "60d54c66eb6cf52bb511",
+    "url": "/static/js/2.ae6dacf0.chunk.js"
+  },
+  {
+    "revision": "91c00eb92e61c3094822",
+    "url": "/static/js/main.1febff0c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
